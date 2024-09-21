1433621182763b4e132f70758c5c7a31fb2f4b6c
Vijay Singh <vijayakabajo@gmail.com>
2024-09-21 13:22:52 +0530

    updated

--- operations/models.py
+++ operations/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Purchase(models.Model):
-    purchase_number = models.IntegerField(blank=False, null=False)
+    purchase_number = models.IntegerField(blank=False, null=False, unique=True)
     supplier_name = models.CharField(max_length=100, null=False, blank=False)
-    
+    item_name = models.CharField(max_length=100, null=False, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False )
+    total = models.DecimalField(max_digits=10, decimal_places=)
--- operations/views.py
+++ operations/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .models import Purchase
 
 # Create your views here.
+# purchase_page 
 def purchase_page(request):
     return render(request, 'purchase.html')
-
+# sales_page
 def sales_page(request):
     return render(request, 'sales.html')
 
