555526061c4d8ece3a2614cdb91a277b4e9cc925
Vijay Singh <vijayakabajo@gmail.com>
2024-09-26 12:01:54 +0530

    Updated 100000th time

--- operations/models.py
+++ operations/models.py
@@ -36,7 +36,7 @@ class PurchaseDetail(models.Model):
 
 
 class TempTable(models.Model):
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE, unique=True)
     quantity = models.IntegerField(null=False, blank=False)
     items_total = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True) 
