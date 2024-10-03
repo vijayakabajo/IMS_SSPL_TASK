cee4f8bb32d0acad538708d807d505a92f536c7a
Vijay Singh <vijayakabajo@gmail.com>
2024-10-03 12:47:38 +0530

    pre-final

--- reports/views.py
+++ reports/views.py
@@ -41,16 +41,16 @@ def report_page(request):
 
 
 
+
 def detailed_list(request):
     items = Item.objects.filter(status=1)
     
     # Get query params
     item_name = request.GET.get('item', '')
-    from_date = request.GET.get('fromdate', '1900-01-01')  # Default to earliest date
-    to_date = request.GET.get('todate', str(datetime.now().date()))  # Default to current date
-    report_type = request.GET.get('type', 'purchase')  # Default to purchase type
+    from_date = request.GET.get('fromdate', '1900-01-01') 
+    to_date = request.GET.get('todate', str(datetime.now().date())) 
+    report_type = request.GET.get('type', 'purchase')
 
-    # Convert string to date
     from_date = parse_date(from_date) or datetime.strptime('1900-01-01', '%Y-%m-%d').date()
     to_date = parse_date(to_date) or datetime.now().date()
 
@@ -58,23 +58,23 @@ def detailed_list(request):
 
     if report_type == 'purchase':
         # Fetch purchase data
-        purchases = PurchaseDetail.objects.filter(created_at__date__range=[from_date, to_date])
+        purchases = PurchaseDetail.objects.filter(created_at__date__range=[from_date, to_date]).order_by('-created_at')
 
         if item_name:
             purchases = purchases.filter(item_id__name__icontains=item_name)
 
         for purchase in purchases:
             stock_data.append({
-                'item_name': purchase.item_id.name,  # Updated for ForeignKey
+                'item_name': purchase.item_id.name,
                 'quantity': purchase.quantity,
                 'total': purchase.items_total,
-                'created_at': purchase.created_at,
+                'created_at': purchase.created_at.strftime('%d-%m-%Y'),
                 'supplier_or_customer': purchase.purchase_master.supplier_id.name
             })
 
     elif report_type == 'sales':
         # Fetch sales data
-        sales = SalesDetail.objects.filter(created_at__date__range=[from_date, to_date])
+        sales = SalesDetail.objects.filter(created_at__date__range=[from_date, to_date]).order_by('-created_at')
 
         if item_name:
             sales = sales.filter(item_id__name__icontains=item_name)
@@ -84,14 +84,13 @@ def detailed_list(request):
                 'item_name': sale.item.name,
                 'quantity': sale.quantity,
                 'total': sale.items_total,
-                'created_at': sale.created_at,
+                'created_at': sale.created_at.strftime('%d-%m-%Y'),  #will change it to bill_date
                 'supplier_or_customer': sale.sales_master.seller.name
             })
 
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return JsonResponse({'stock_data': stock_data})
 
-    # Otherwise, render the template normally
     context = {
         'stock_data': stock_data,
         'items': items
